d24b8d2ccf6e10002236f2f22063bbd9faa680c2
Dilyan Damyanov <dilyand@gmail.com>
2018-12-07 18:35:10 +0000

    Try something

--- .travis/simple-deploy/deploy.sh
+++ .travis/simple-deploy/deploy.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
 set -e
+
+mkdir -p ~/.aws
+
+cat > ~/.aws/credentials << EOL
+[default]
+aws_access_key_id = ${AWS_ACCESS_KEY_ID}
+aws_secret_access_key = ${AWS_SECRET_ACCESS_KEY}
+EOL
+
 cd $TRAVIS_BUILD_DIR
 pwd
 sbt assembly
-aws s3 ls
-aws s3 cp ./target/scala-2.11 s3://dilyan-test-lambda/travis-builds --recursive --exclude "*" --include "*.jar"
+aws --region eu-west-2 s3 ls
+aws --region eu-west-2 s3 cp ./target/scala-2.11 s3://dilyan-test-lambda/travis-builds --recursive --exclude "*" --include "*.jar"
