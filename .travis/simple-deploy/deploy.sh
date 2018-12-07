f4181c3927ca209011395f22da47675c0b0313d1
Dilyan Damyanov <dilyand@gmail.com>
2018-12-07 16:56:31 +0000

    Update aws cli command

--- .travis/simple-deploy/deploy.sh
+++ .travis/simple-deploy/deploy.sh
@@ -2,12 +2,8 @@
 
 set -e
 
-tag=$1
-
 cd $TRAVIS_BUILD_DIR
-
-export TRAVIS_BUILD_RELEASE_TAG=${tag}
 
 sbt assembly
 
-aws s3 cp ./target/scala-2.11/toffee-box-${TRAVIS_BUILD_RELEASE_TAG}.jar s3://dilyan-test-lambda
+aws s3 cp ./target/scala-2.11 s3://dilyan-test-lambda/travis-builds --recursive --exclude "*" --include "*.jar"
