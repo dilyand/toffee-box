08e2208c00dbffb5e4c5e588cb459aee78e6b8da
Dilyan Damyanov <dilyand@gmail.com>
2018-12-08 18:14:22 +0000

    --pro fixes it

--- .travis/simple-deploy/deploy.sh
+++ .travis/simple-deploy/deploy.sh
@@ -4,9 +4,5 @@ set -e
 
 cd $TRAVIS_BUILD_DIR
 pwd
-echo ${AWS_ACCESS_KEY_ID}
-echo ${AWS_SECRET_ACCESS_KEY}
-echo ${AWS_DEFAULT_REGION}
 sbt assembly
-aws --region eu-west-2 s3 ls
-aws --region eu-west-2 s3 cp ./target/scala-2.11 s3://dilyan-test-lambda/travis-builds --recursive --exclude "*" --include "*.jar"
+aws --region $AWS_DEFAULT_REGION s3 cp ./target/scala-2.11 s3://dilyan-test-lambda/travis-builds --recursive --exclude "*" --include "*.jar"
