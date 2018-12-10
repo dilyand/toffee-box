74f9d89c46b67812c7c932e38c6af27951330b9d
Dilyan Damyanov <dilyand@gmail.com>
2018-12-10 16:05:48 +0000

    Bashing

--- .travis/simple-deploy/deploy.sh
+++ .travis/simple-deploy/deploy.sh
@@ -5,13 +5,15 @@ set -e
 cd $TRAVIS_BUILD_DIR
 pwd
 
-if [[$TRAVIS_TAG == toffee-box*]]; then
-  cd ./toffee-box
+if [[ $TRAVIS_TAG =~ ^toffee-box.* ]]
+then
+  cd toffee-box
 else
   echo "Wrong tag."
 fi
 
 pwd
+ls
 
 sbt assembly
 aws --region $AWS_DEFAULT_REGION s3 cp ./target/scala-2.11 s3://dilyan-test-lambda/travis-builds --recursive --exclude "*" --include "*.jar"
